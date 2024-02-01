a1fdd43d46e61dd1a58ff80138e84f5e9e1d204d
pieceofGithub <demiroglu.webworld@gmail.com>
2024-02-01 14:36:26 +0300

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 const app = express()
 
-app.listen(3000, () => {
-    console.log("server running!!!éé");
-})
+
+const connectionOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex ve useFindAndModify seçenekleri kaldırıldı
+  };
+  
+  // MongoDB bağlantı URL'sine ek seçenekleri ekleyin
+  mongoose.connect(process.env.MONGO_DB_URI, connectionOptions)
+    .then(() => {
+      console.log('Connected to MongoDB');
+      app.listen(3000, () => {
+          console.log("server running!!!éé");
+      })
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error.message);
+    });
+    
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,10 +13,10 @@ export default function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/kaydol" element={<SignIn/>} />
         <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/hakkında" element={<About/>} />
+        <Route path="/kullanıcı" element={<Profile/>} />
 
         
       </Routes>
