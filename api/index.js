2b4fa144c08b157c6d70a0cc3c106658ae6936b1
pieceofGithub <demiroglu.webworld@gmail.com>
2024-02-01 15:48:47 +0300

    create test route

--- api/index.js
+++ api/index.js
@@ -1,25 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config()
 const app = express()
 
 
 const connectionOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex ve useFindAndModify seçenekleri kaldırıldı
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex ve useFindAndModify seçenekleri kaldırıldı
   };
   
   // MongoDB bağlantı URL'sine ek seçenekleri ekleyin
   mongoose.connect(process.env.MONGO_DB_URI, connectionOptions)
-    .then(() => {
-      console.log('Connected to MongoDB');
-      app.listen(3000, () => {
-          console.log("server running!!!éé");
-      })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+      console.log("server running!!!éé");
     })
-    .catch((error) => {
-      console.error('Error connecting to MongoDB:', error.message);
-    });
-    
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+  
+  app.use('/api/user', userRouter)
