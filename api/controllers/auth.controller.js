cfd2e4fdf57a67a9a23a2895502c5f67c2fae275
pieceofGithub <demiroglu.webworld@gmail.com>
2024-02-02 13:52:45 +0300

    create mddleware and a function error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async(req, res) => {
+export const signup = async(req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password,10)
   
@@ -15,6 +16,6 @@ export const signup = async(req, res) => {
     })
     .catch((error) => {
       console.error("Kullanıcı kaydı sırasında bir hata oluştu:", error);
-      res.status(500).json({ message: "Kullanıcı kaydı sırasında bir hata oluştu." });
+      next(error);
     });
 };
--- api/index.js
+++ api/index.js
@@ -28,3 +28,15 @@ const connectionOptions = {
   
   app.use('/api/user', userRouter)
   app.use('/api/auth',authRouter)
+  
+  app.use((err, req, res, next) => {
+    
+    const statusCode = err.statusCode || 500;
+    const errorMessage = err.message || "Internal Server Error";
+
+    return res.status(statusCode).json({
+      success:"false",
+      statusCode,
+      errorMessage
+    })
+  })
